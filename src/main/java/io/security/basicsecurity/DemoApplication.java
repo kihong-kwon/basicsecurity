ae069906baa634167019f313fa299f3bbd9cbdff
kihong-kwon <kihong.kwon7389@gmail.com>
2020-05-11 21:56:56 +0900

    Initial commit

---
+++ src/main/java/io/security/basicsecurity/DemoApplication.java
@@ -0,0 +1,13 @@
+package io.security.basicsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+}
---
+++ src/main/java/io/security/basicsecurity/SecurityController.java
@@ -0,0 +1,13 @@
+package io.security.basicsecurity;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class SecurityController {
+
+    @GetMapping("/loginPage")
+    public String loginPage() {
+        return "loginPage";
+    }
+}
